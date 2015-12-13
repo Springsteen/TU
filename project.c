e40200596776963dae333178913f92097aa93f00
Martin Ivanov <martin.iv96@gmail.com>
2015-12-13 16:28:09 +0200

    Extend project:  - Count if, if/else from file  - Write results to file or stdout

--- project.c
+++ project.c
@@ -1,20 +1,23 @@
 #include "stdio.h"
+#include "string.h"
 
 #define FLUSH while (getchar() != '\n') // replaces fflush on linux
 
-int makeCalculations (char *finput, char *foutput);
+int searchThroughString (char *string, char *pattern);
+int readFromFile (const char *finput, const char *foutput);
+void writeResults (const char *fileout, int ifCount, int elseCount, int charsCount);
 
 int main (int argc, char const *argv[]) {
 
     int optionChoosen = 0, read = 0;
 
-    printf("Изберете начин на работа \n");
-    printf("1 - Четене от файл, изход във файл \n");
-    printf("2 - Четене от файл, изход на екрана \n");
-    printf("3 - Четене от клавиатура, изход на екрана \n");
-    printf("4 - Четене от клавиатура, във файл \n");
-
     while (1) {
+        printf("Изберете начин на работа \n");
+        printf("1 - Четене от файл, изход във файл \n");
+        printf("2 - Четене от файл, изход на екрана \n");
+        printf("3 - Четене от клавиатура, изход на екрана \n");
+        printf("4 - Четене от клавиатура, във файл \n");
+
         read = scanf("%d", &optionChoosen);
 
         if (read > 0) {
@@ -33,9 +36,20 @@ int main (int argc, char const *argv[]) {
 
     switch (optionChoosen) {
         case 1:
-            makeCalculations("read.txt", NULL);
+            if (argc >= 3) {
+                readFromFile(argv[1], argv[2]);
+            } else {
+                printf("Не сте въвели файлове за четене и писане\n");
+                return 0;
+            }
             break;
         case 2:
+            if (argc >= 2) {
+                readFromFile(argv[1], NULL);
+            } else {
+                printf("Не сте въвели файл за четене\n");
+                return 0;
+            }
             break;
         case 3:
             break;
@@ -47,22 +61,80 @@ int main (int argc, char const *argv[]) {
 }
 
 
-int makeCalculations (char *finput, char *foutput) {
-    if (finput) {
-        FILE *input;
-        char *line = NULL;
-        size_t len = 0;
-        ssize_t charactersRead;
-
-        input = fopen(finput, "r");
-
-        while ((charactersRead = getline(&line, &len, input)) != -1) {
-            printf("Прочетения ред е с дължина %zu :\n", charactersRead);
-            printf("%s", line);
-        }
-
-        fclose(input);
+int readFromFile (const char *finput, const char *foutput) {
+    if (!finput) {
+        printf("Не сте въвели файл за четене\n");
+        return 0;
     }
 
+    FILE *input;
+
+    // file read vars
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t charactersRead;
+
+    // comment vars
+    const char pattern[3] = "/*";
+    char *token;
+    int inComment = 0;
+
+    // result vars
+    int ifCount = 0;
+    int elseCount = 0;
+    int charsCount = 0;
+
+    input = fopen(finput, "r");
+
+    while ((charactersRead = getline(&line, &len, input)) != -1) {
+        charsCount += charactersRead;
+
+        token = strtok(line, pattern);
+        if (token != NULL) {
+            while( token != NULL ) {
+                if (!inComment) {
+                    ifCount += searchThroughString(token, "if");
+                    elseCount += searchThroughString(token, "else");
+                }
+                token = strtok(NULL, pattern);
+                if (token != NULL) inComment = !inComment ;
+            }
+        }
+    }
+
+    fclose(input);
+
+    writeResults(foutput, ifCount, elseCount, charsCount);
+
     return 0;
+}
+
+void writeResults (const char *fileout, int ifCount, int elseCount, int charsCount) {
+    if (fileout) {
+        FILE *fp;
+        fp = fopen(fileout, "w");
+
+        fprintf(fp, "Брой символи %d\n", charsCount);
+        fprintf(fp, "Брой if-ове %d\n", ifCount);
+        fprintf(fp, "Брой else-ове %d\n", elseCount);
+
+        fclose(fp);
+    } else {
+        printf("Брой символи %d\n", charsCount);
+        printf("Брой if-ове %d\n", ifCount);
+        printf("Брой else-ове %d\n", elseCount);
+    }
+}
+
+
+int searchThroughString (char *string, char *pattern) {
+    int count = 0;
+    int length = strlen(pattern);
+
+    if (!length) return 0;
+
+    for (string = strstr(string, pattern); string; string = strstr(string + length, pattern))
+        ++count;
+
+    return count;
 }
